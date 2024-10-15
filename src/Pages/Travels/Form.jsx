7299955d81c3041f9b1b9ff6831ae6993d14037e
Leonardo Valcarenghi <leonardo@esgnow.co>
2024-10-15 09:49:27 -0300

    feat - renderização e atualização de states na tela de form de viagens

--- src/Pages/Travels/Form.jsx
+++ src/Pages/Travels/Form.jsx
@@ -1,16 +1,21 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { TRAVEL } from "../../Models/Travel";
 import { GetTravel } from "../../Services/Travels";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import PageHeader from "../../Components/Header";
-import { IconCheck, IconPlus, IconTrash } from "@tabler/icons-react";
+import { IconBuildingAirport, IconCheck, IconPlaneArrival, IconPlus, IconTrash } from "@tabler/icons-react";
+import { GetFederativeUnits } from "../../Services/FederativeUnits";
 
 export default function Travels_FormPage() {
 
     const { identifier } = useParams();
     const navigate = useNavigate();
+    const lastDestinationRef = useRef(null); // Referência para o último destino
 
     const [travel, setTravel] = useState(TRAVEL);
+
+    const [federativeUnits, setFederativeUnits] = useState([]);
+    const [cities, setCities] = useState([]);
 
     const [importing, setImporting] = useState(false);
     const [processing, setProcessing] = useState(false);
@@ -18,7 +23,13 @@ export default function Travels_FormPage() {
     const [errorOnImport, setErrorOnImport] = useState(false);
     const [errorOnProcess, setErrorOnProcess] = useState(false);
 
+    useEffect(() => { getFederativeUnits(); }, []);
     useEffect(() => { getTravel(); }, [identifier]);
+
+    async function getFederativeUnits() {
+        const result = await GetFederativeUnits();
+        setFederativeUnits(result);
+    }
 
     async function getTravel() {
         setImporting(true);
@@ -32,11 +43,10 @@ export default function Travels_FormPage() {
     }
 
     async function saveTravel() {
-
+        // Lógica para salvar a viagem
     }
 
     function addDestination() {
-
         const destination = {
             "idMunicipioDestino": 101,
             "DataDestinoViagem": "2024-01-05",
@@ -44,15 +54,47 @@ export default function Travels_FormPage() {
                 "idTipoCusto": 1,
                 "ValorCustoDestino": 150.5
             }
-        }
+        };
+        setTravel(_ => ({ ..._, destinos: [..._.destinos, destination] }));
 
-        setTravel(_ => ({ ..._, destinos: [..._.destinos, destination] }))
+        // Após adicionar, rola a tela até o último card
+        setTimeout(() => {
+            if (lastDestinationRef.current) {
+                lastDestinationRef.current.scrollIntoView({ behavior: 'smooth' });
+            }
+        }, 0); // O timeout garante que o DOM seja atualizado antes de tentar rolar.
     }
 
-    function removeDestination(destination) {
+    function removeDestination(index) {
+        setTravel(prevTravel => ({
+            ...prevTravel,
+            destinos: prevTravel.destinos.filter((_, i) => i !== index)
+        }));
+    }
 
-        const dest = travel.destinos.find(destination);
-        console.log('dest>>>>>>>>>>>>>>>>>', dest);
+    function updateDestination(index, field, value) {
+        setTravel(prevTravel => ({
+            ...prevTravel,
+            destinos: prevTravel.destinos.map((destination, i) =>
+                i === index
+                    ? { ...destination, [field]: value }
+                    : destination
+            )
+        }));
+    }
+
+    function updateCost(index, newValue) {
+        setTravel(prevTravel => ({
+            ...prevTravel,
+            destinos: prevTravel.destinos.map((destination, i) =>
+                i === index
+                    ? {
+                        ...destination,
+                        custo: { ...destination.custo, ValorCustoDestino: newValue }
+                    }
+                    : destination
+            )
+        }));
     }
 
     return (
@@ -68,25 +110,18 @@ export default function Travels_FormPage() {
 
             <div className="row mb-3">
                 <div className="col">
-                    <div className="card">
+                    <div className="card border-primary">
                         <div className="card-header">
                             <div className="row align-items-center">
                                 <div className="col">
-                                    <h6 className="card-title text-uppercase mb-0">Origem</h6>
-                                </div>
-                                <div className="col">
-                                    <button type="button" className="btn btn-sm float-end px-2" onClick={addDestination}>
-                                        <IconPlus className="icon" stroke={1} />
-                                        Adicionar Destino
-                                    </button>
+                                    <h6 className="card-title text-uppercase mb-0">
+                                        <IconBuildingAirport className="me-2" size={36} stroke={1} />
+                                        Origem
+                                    </h6>
                                 </div>
                             </div>
                         </div>
                         <div className="card-body">
-
-
-
-
                             <div className="row mb-3">
                                 <div className="col">
                                     <label className="form-label required" htmlFor="nameInput">Unidade Federativa:</label>
@@ -98,7 +133,6 @@ export default function Travels_FormPage() {
                                 </div>
                             </div>
 
-
                             <div className="row">
                                 <div className="col">
                                     <label className="form-label required" htmlFor="nameInput">Data Inicio:</label>
@@ -109,110 +143,108 @@ export default function Travels_FormPage() {
                                     <input type="date" className="form-control" />
                                 </div>
                             </div>
-
-
                         </div>
                     </div>
                 </div>
             </div>
 
-
-            {
-                travel.destinos.map(destination =>
-                    <div className="row mb-3">
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-header">
-                                    <div className="row align-items-center">
-                                        <div className="col">
-                                            <h6 className="card-title text-uppercase mb-0">Destino 01</h6>
-                                        </div>
-                                        <div className="col">
-                                            <button type="button" className="btn btn-sm float-end px-2" onClick={() => removeDestination(destination)}>
-                                                <IconTrash className="icon" stroke={1} />
-                                                Remover Destino
-                                            </button>
-                                        </div>
+            {travel.destinos.map((destination, index) => (
+                <div className="row mb-3" key={index} ref={index === travel.destinos.length - 1 ? lastDestinationRef : null}>
+                    <div className="col">
+                        <div className="card border-success" >
+                            <div className="card-header">
+                                <div className="row align-items-center">
+                                    <div className="col">
+                                        <h6 className="card-title text-uppercase mb-0">
+                                            <IconPlaneArrival className="me-2" size={36} stroke={1} />
+                                            Destino {index + 1}
+                                        </h6>
+                                    </div>
+                                    <div className="col">
+                                        <button type="button" className="btn btn-sm float-end px-2" onClick={() => removeDestination(index)}>
+                                            <IconTrash className="icon" stroke={1} />
+                                            Remover Destino
+                                        </button>
                                     </div>
                                 </div>
-                                <div className="card-body">
-
-
-
-                                    {/* Origem */}
-                                    <div className="row mb-3">
-                                        <div className="col">
-                                            <label className="form-label required" htmlFor="nameInput">Unidade Federativa:</label>
-                                            <select className="form-control"></select>
-                                        </div>
-                                        <div className="col">
-                                            <label className="form-label required" htmlFor="nameInput">Cidade:</label>
-                                            <select className="form-control"></select>
-                                        </div>
+                            </div>
+                            <div className="card-body">
+                                <div className="row mb-3">
+                                    <div className="col">
+                                        <label className="form-label required" htmlFor={`uf_${index}`}>Unidade Federativa:</label>
+                                        <select
+                                            id={`uf_${index}`}
+                                            className="form-control"
+                                            value={destination.idMunicipioDestino}
+                                            onChange={(e) => updateDestination(index, 'idMunicipioDestino', e.target.value)}
+                                        >
+                                            {/* Opções de UF */}
+                                        </select>
                                     </div>
-
-                                    <div className="row mb-lg-3">
-                                        <div className="col">
-                                            <label className="form-label required" htmlFor="nameInput">Data:</label>
-                                            <input type="date" className="form-control" />
-                                        </div>
+                                    <div className="col">
+                                        <label className="form-label required" htmlFor={`cidade_${index}`}>Cidade:</label>
+                                        <select
+                                            id={`cidade_${index}`}
+                                            className="form-control"
+                                            value={destination.idMunicipioDestino}
+                                            onChange={(e) => updateDestination(index, 'idMunicipioDestino', e.target.value)}
+                                        >
+                                            {/* Opções de Cidade */}
+                                        </select>
                                     </div>
+                                </div>
 
-
-                                    {/* Custos */}
-                                    <div className="row">
-                                        <div className="col-12 mb-2">
-                                            <h6 className=" text-uppercase">Custos</h6>
-                                        </div>
-                                        <div className="col">
-                                            <table className="table table-bordered">
-                                                <tbody>
-                                                    <tr>
-                                                        <td>
-                                                            Passagem
-                                                        </td>
-                                                        <td>
-                                                            <input type="text" className="form-control" placeholder="R$ 0,00" />
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>
-                                                            Alimentação
-                                                        </td>
-                                                        <td>
-                                                            <input type="text" className="form-control" placeholder="R$ 0,00" />
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>
-                                                            Hospedagem
-                                                        </td>
-                                                        <td>
-                                                            <input type="text" className="form-control" placeholder="R$ 0,00" />
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>
-                                                            Transporte
-                                                        </td>
-                                                        <td>
-                                                            <input type="text" className="form-control" placeholder="R$ 0,00" />
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
+                                <div className="row mb-3">
+                                    <div className="col">
+                                        <label className="form-label required" htmlFor={`data_${index}`}>Data:</label>
+                                        <input
+                                            type="date"
+                                            id={`data_${index}`}
+                                            className="form-control"
+                                            value={destination.DataDestinoViagem}
+                                            onChange={(e) => updateDestination(index, 'DataDestinoViagem', e.target.value)}
+                                        />
                                     </div>
+                                </div>
 
+                                <div className="row">
+                                    <div className="col-12 mb-2">
+                                        <h6 className="text-uppercase">Custos</h6>
+                                    </div>
+                                    <div className="col">
+                                        <table className="table table-bordered">
+                                            <tbody>
+                                                <tr>
+                                                    <td>Passagem</td>
+                                                    <td>
+                                                        <input
+                                                            type="text"
+                                                            className="form-control"
+                                                            placeholder="R$ 0,00"
+                                                            value={destination.custo?.ValorCustoDestino || ''}
+                                                            onChange={(e) => updateCost(index, e.target.value)}
+                                                        />
+                                                    </td>
+                                                </tr>
+                                                {/* Outros campos de custos */}
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                )
-            }
+                </div>
+            ))}
 
-
+            <div className="row justify-content-center">
+                <div className="col-auto">
+                    <button type="button" className="btn btn-sm btn-outline-secondary" onClick={addDestination}>
+                        <IconPlus className="icon" stroke={1} />
+                        Adicionar Destino
+                    </button>
+                </div>
+            </div>
         </>
-    )
-
+    );
 }
