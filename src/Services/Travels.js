de6a35be76370da185546160f152d208d34ec74d
Jonathan Bockorny <jonathan.igor.pereira@hotmail.com>
2024-10-20 00:46:36 -0300

    implementação do oAuth + correções nos status + função para download

--- src/Services/Travels.js
+++ src/Services/Travels.js
@@ -3,7 +3,7 @@ import axios from "axios";
 /**
  * Cadastrar novo viagem.
  * @param {*} data Modelo.
- * @returns 
+ * @returns
  */
 export const NewTravel = (data = {}) => axios.post("/viagens", data);
 
@@ -11,48 +11,63 @@ export const NewTravel = (data = {}) => axios.post("/viagens", data);
  * Editar viagem.
  * @param {*} id Identificador da viagem.
  * @param {*} data Modelo.
- * @returns 
+ * @returns
  */
-export const EditTravel = (id = 0, data = {}) => axios.put(`/viagens/${id}`, data);
+export const EditTravel = (id = 0, data = {}) =>
+  axios.put(`/viagens/${id}`, data);
 
 /**
  * Excluir viagem.
  * @param {*} id Identificador da viagem.
- * @returns 
+ * @returns
  */
 export const DeleteTravel = (id = 0) => axios.delete(`/viagens/${id}`);
 
 /**
  * Obter uma viagem.
  * @param {*} id Identificador da viagem.
- * @returns 
+ * @returns
  */
 export const GetTravel = (id = 0) => axios.get(`/viagens/${id}`);
 
 /**
  * Obter todos as viajens cadastrados para um usuário.
  * @param {*} userID Identificador do usuário.
- * @returns 
+ * @returns
  */
 export const GetTravels = (userID) => axios.get(`/viagens/empregado/${userID}`);
 
 /**
  * Obter PDF da viajem.
  * @param {*} id Identificador da viagem.
- * @returns 
+ * @returns
  */
-export const GetTravelPDF = (id) => axios.get(`/viagens/${id}/exportar-pdf`);
+//export const GetTravelPDF = (id) => axios.get(`/viagens/${id}/exportar-pdf`);
+export async function GetTravelPDF(idViagem, token) {
+  try {
+    const response = await axios.get(`/viagens/${idViagem}/exportar-pdf`, {
+      responseType: "blob",
+      //   headers: {
+      //     Authorization: `Bearer ${token}`,
+      //   },
+    });
+    return response;
+  } catch (error) {
+    console.log("error=>", error);
+    throw error;
+  }
+}
 
 /**
  * Aprovar viagem.
  * @param {*} id Identificador da viagem.
- * @returns 
+ * @returns
  */
 export const ApproveTravel = (id) => axios.put(`/viagens/${id}/aprovar`);
 
 /**
  * Rejeitar viagem.
  * @param {*} id Identificador da viagem.
- * @returns 
+ * @returns
  */
 export const RejectTravel = (id) => axios.put(`/viagens/${id}/reprovar`);
